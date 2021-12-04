d7de03d8e855c91df8f4fa8122d8981527645174
HienDX124 <xuanhien2000678354@gmail.com>
2021-12-04 07:00:08 +0700

    Lesson 19: Item Details Class: Done

--- Assets/Scripts/Enums/Enums.cs
+++ Assets/Scripts/Enums/Enums.cs
@@ -11,3 +11,18 @@ public enum Direction
     left,
     right
 }
+
+public enum ItemType
+{
+    Seed,
+    Commondity,
+    Watering_tool,
+    Hoeing_tool,
+    Chopping_tool,
+    Breaking_tool,
+    Reaping_tool,
+    Collecting_tool,
+    Reapable_scenary,
+    none,
+    count
+}
---
+++ Assets/Scripts/Item/ItemDetails.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ItemDetails : MonoBehaviour
+{
+    public int itemCode;
+    public ItemType itemType;
+    public string itemDescription;
+    public Sprite itemSprite;
+    public string itemLongDescription;
+    public short itemUseGridRadius;
+    public float itemUseRadius;
+    public bool isStartingItem;
+    public bool canPickedUp;
+    public bool canBeDroped;
+    public bool canBeEaten;
+    public bool canBeCarried;
+
+}
