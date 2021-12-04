33584c7424b792222bb3996c9f4d291ed7225e90
HienDX124 <xuanhien2000678354@gmail.com>
2021-12-04 12:17:02 +0700

    Lesson 22: InventoryManager class

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InventoryManager : SingletonMonobehaviour<InventoryManager>
+{
+
+    private Dictionary<int, ItemDetails> itemDetailsDictionary;
+
+    [SerializeField] private SO_ItemList itemList = null;
+
+    private void Start()
+    {
+        // Creat item details dictionary
+        CreatItemDetailsDictionary();
+    }
+
+    private void CreatItemDetailsDictionary()
+    {
+        itemDetailsDictionary = new Dictionary<int, ItemDetails>();
+        foreach (ItemDetails itemDetails in itemList.itemDetails)
+        {
+            itemDetailsDictionary.Add(itemDetails.itemCode, itemDetails);
+        }
+    }
+    /// <sumary>
+    /// Return the itemDetails (from the SO_itemList) for the itemCode, or null if the itemCode doesn't exist
+    /// </sumary>
+
+    public ItemDetails GetItemDetails(int itemCode)
+    {
+        ItemDetails itemDetails;
+
+        if (itemDetailsDictionary.TryGetValue(itemCode, out itemDetails))
+        {
+            return itemDetails;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Player/ItemPickUp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Item item = collision.GetComponent<Item>();
+        if (item != null)
+        {
+            // Get item details
+            ItemDetails itemDetails = InventoryManager.Instance.GetItemDetails(item.ItemCode);
+
+            // Print item description to console window
+            Debug.Log(itemDetails.itemDescription);
+        }
+    }
+}
